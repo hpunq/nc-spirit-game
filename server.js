02c42bfadd3b0c695f5c36c44ccbf623ddd125d1
Hasan-M12 <mhasan259@outlook.com>
2025-01-14 15:08:28 +0000

    working on mutliclient connections

--- server.js
+++ server.js
@@ -1,26 +1,50 @@
-import express from 'express';
+import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-const app = express()
-const server = createServer(app);
-const io = new Server(server);
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-    console.log('a user connected');
+  console.log("A connection has been made!")
 });
 
 httpServer.listen(3000);
+// const io = require('socket.io')(3000)
 
-function getAllPlayers(){
-    var players = [];
-    Object.keys(io.sockets.connected).forEach(function(socketID){
-        var player = io.sockets.connected[socketID].player;
-        if(player) players.push(player);
-    });
-    return players;
-}
+// io.on('connection', socket => {
+//     console.log(socket.id)
+// })
 
-function randomInt (low, high) {
-    return Math.floor(Math.random() * (high - low) + low);
-}
+// const app = express();
+// const io = new Server(httpServer);
+
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.sendFile("Hello world!");
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.emit("serverToClient", "Hello client!");
+//   socket.on('clientToServer', data => {
+//     console.log(data)
+//   })
+// });
+
+// httpServer.listen(port);
+
+// function getAllPlayers() {
+//   var players = [];
+//   Object.keys(io.sockets.connected).forEach(function (socketID) {
+//     var player = io.sockets.connected[socketID].player;
+//     if (player) players.push(player);
+//   });
+//   return players;
+// }
+
+// function randomInt(low, high) {
+//   return Math.floor(Math.random() * (high - low) + low);
+// }
--- src/client.js
+++ src/client.js
@@ -1,20 +1,5 @@
-var Client = {};
-Client.socket = io.connect();
+const socket = io('https://localhost:3000')
 
-Client.socket.on('newplayer',function(data){
-    Game.addNewPlayer(data.id,data.x,data.y);
-});
-
-Client.socket.on('allplayers',function(data){
-    for(var i = 0; i < data.length; i++){
-        Game.addNewPlayer(data[i].id,data[i].x,data[i].y);
-    }
-
-    Client.socket.on('move',function(data){
-        Game.movePlayer(data.id,data.x,data.y);
-    });
-
-    Client.socket.on('remove',function(id){
-        Game.removePlayer(id);
-    });
-});
+socket.on('serverToClient', data => {
+    alert(data)
+})
